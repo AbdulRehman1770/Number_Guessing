c768fa00b7606bc85f72b9ad58c2d0a94503195c
AbdulRehman1770 <rehman71gtx@gmail.com>
2024-06-16 18:01:04 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer"
+
+let Random_Number : number = Math.floor(Math.random()*10)
+console.log("You Have 5 Attempts");
+
+let z = 0
+while(z < 5){
+    let Guess_Number = await inquirer.prompt({
+    message : "Enter Your Number",
+    type : "number",
+    name : "GuessNumber"  
+})
+
+    if (Guess_Number.GuessNumber === Random_Number){
+        console.log("You are Winner! \nYou Guess Right Number ");
+        break;}
+        
+    else {
+        console.log("You Guess Wrong Number");
+        console.log("Try Again...");}
+    z += 1}
